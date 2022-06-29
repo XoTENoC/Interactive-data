df67ce7ac08b1f5329699398893e84c6b3b8dd67
Nathaniel Chang <changipro.nathaniel@gmail.com>
2022-06-29 22:48:55 +0930

    adding draw_grid(), init variables and cell()

---
+++ ant_sim/script.js
@@ -0,0 +1,37 @@
+var _data; // from the previous frame
+
+// drawing out the grid
+
+function draw_grid(data){
+    var width = 600;
+    var height = 600;
+};
+
+// init variables
+var grid_legth = 150;
+var grid = [];
+var temp_grid = [];
+var population = [];
+var max_ants = 100; // number of ants on the cavas
+var interval = 33;  //  time delay between each of the frame in ms
+
+//functions for converting radians and degrees
+Math.to_radians = function(degrees){
+    return degrees * Math.PI / 180;
+};
+
+Math.to_degrees = function(radians){
+    return radians * 180 / Math.PI;
+};
+
+// function for the cells in the grid
+function cell(_x, _y){
+    this.x = _x;
+    this.y = _y;
+    this.ant = null;
+    this.food = 0;
+    this.signal = 0;
+    this.has_ant = function() {
+        return this.ant ? true : false;
+    };
+}
